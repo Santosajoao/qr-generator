2faf7601be7c56e8d15f49faec3e3913f0567c0c
João Pedro Santos <112666791+Santosajoao@users.noreply.github.com>
2023-05-02 13:37:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector("img"),
+      input = document.querySelector("input"),
+      button = document.querySelector("button"),
+      api = `https://api.qrserver.com/v1/`,
+      api2 = `create-qr-code/?size=150x150&data=`;
+  
+    button.addEventListener("click", () => {
+      image.src = `${api}${api2}${input.value}`;
+    });
+  });
+  
