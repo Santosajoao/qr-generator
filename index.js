bc6e3d7c9cabe5e8fa45865c5d9cecf8c09dd030
Joao Araujo <joaosantos162003@gmail.com>
2026-04-08 12:06:54 -0300

    Melhora a lógica de geração do QR code, permitindo a personalização do tamanho através de inputs

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const image = document.querySelector("img"),
       input = document.querySelector("input"),
+      widthInput = document.querySelector("#width"),
+      heightInput = document.querySelector("#height"),
       button = document.querySelector("button"),
       api = `https://api.qrserver.com/v1/`,
-      api2 = `create-qr-code/?size=150x150&data=`;
+      api2 = `create-qr-code/?size=`,
+      api3 = `&data=`;
   
     button.addEventListener("click", () => {
-      image.src = `${api}${api2}${input.value}`;
+      let data = input.value;
+      let width = widthInput.value;
+      let height = heightInput.value;
+      if (data.length > 0 && width > 0 && height > 0) {
+        let size = `${width}x${height}`;
+        image.src = `${api}${api2}${size}${api3}${data}`;
+      }
     });
   });
   
